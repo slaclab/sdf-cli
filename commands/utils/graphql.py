6f5f4d37dfbe0effdde63c74716e9aaebe9b0b11
yee379 <yee379@gmail.com>
2022-03-25 13:19:57 -0700

    misc updates to graphql class

--- commands/utils/graphql.py
+++ commands/utils/graphql.py
@@ -6,6 +6,10 @@ from gql.transport.aiohttp import AIOHTTPTransport
 from cliff.lister import Lister
 
 import logging
+from gql.transport.requests import log as requests_logger
+requests_logger.setLevel(logging.ERROR)
+from gql.transport.websockets import log as websockets_logger
+websockets_logger.setLevel(logging.ERROR)
 
 
 SDF_IRIS_URI=getenv("SDF_IRIS_URI", "http://localhost:8000/graphql")
@@ -20,11 +24,11 @@ class GraphQlLister(Lister):
         super(GraphQlLister, self).__init__(app, app_args, cmd_name=cmd_name)
         self.connect( SDF_IRIS_URI )
 
-    def connect(self, graphql_uri ):
+    def connect(self, graphql_uri, get_schema=False ):
         self.transport = AIOHTTPTransport(url=graphql_uri)
-        self.client = Client(transport=self.transport, fetch_schema_from_transport=True)
+        self.client = Client(transport=self.transport, fetch_schema_from_transport=get_schema)
 
-    def query(self, query ):
-        return self.client.execute( gql(query) )
+    def query(self, query, var={} ):
+        return self.client.execute( gql(query), variable_values=var )
 
 
