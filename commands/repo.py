76e3ff024323cbfdb2997df49685944a82d89d88
yee379 <yee379@gmail.com>
2022-03-26 01:10:50 -0700

    feat: added some stub commands

--- commands/repo.py
+++ commands/repo.py
@@ -9,13 +9,14 @@ from .utils.graphql import GraphQlLister
 class List(GraphQlLister):
     "show list of Repo's available"
 
-    LOG = logging.getLogger(__name__)
-
     def take_action(self, parsed_args):
+
+
+        # TODO: add filtering
+
         res = self.query("""
         { repos { name gid users } }
         """)
-        self.LOG.warning(f"OUTPUT {res}")
 
         # filter example
         # { repos(filters:{ name: "bd" } ) { name gid users }}
@@ -26,6 +27,34 @@ class List(GraphQlLister):
             ((r['name'], r['gid'], ','.join(r['users'][0:10])) for r in res['repos'])
         )
 
+
+class GetReposWithUser(GraphQlLister):
+    "list the Repos a User is associated with"
+
+
+class ListMine(GraphQlLister):
+    "show list of Repos that you have Principal or Leader roles on"
+
+class AddUser(GraphQlLister):
+    "add a User to a Repo (you must have Principal or Leader role on the Repo)"
+
+
+class SyncToLDAP(GraphQlLister):
+    "produce LDIFs of Repo information suitable for LDAP groups"
+    def get_parser(self, prog_name):
+        parser = super(SyncToLDAP, self).get_parser(prog_name)
+        parser.add_argument('dn', default='ou=Group,dc=reg,o=slac')
+        return parser
+
+    def take_action(self, parsed_args):
+        self.LOG.warn(f"{parsed_args}")
+        res = self.query("""
+        { repos { name gid users } }
+        """)
+
+
+
+
 class Repo(CommandManager):
     "Manage Repo's"
 
@@ -33,7 +62,7 @@ class Repo(CommandManager):
 
     def __init__(self, namespace, convert_underscores=True):
         super(Repo,self).__init__(namespace, convert_underscores=convert_underscores)
-        for cmd in [ List, ]:
+        for cmd in [ List, SyncToLDAP ]:
             self.add_command( cmd.__name__.lower(), cmd )
 
 
