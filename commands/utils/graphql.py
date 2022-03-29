76c4ef89f1c22d5561f11abc9cc5ee23aeaa189f
Yee-Ting Li <yee379@gmail.com>
2022-03-29 11:44:55 -0700

    refactor: separate out graphql functionality from Command

--- commands/utils/graphql.py
+++ commands/utils/graphql.py
@@ -14,21 +14,26 @@ websockets_logger.setLevel(logging.ERROR)
 
 SDF_IRIS_URI=getenv("SDF_IRIS_URI", "http://localhost:8000/graphql")
 
-class GraphQlLister(Lister):
-    "Example of how to fetch data from graphql and dump output as a table"
-    LOG = logging.getLogger(__name__)
+
+class GraphQlClient:
+
     transport = None
     client = None
 
-    def __init__(self, app, app_args, cmd_name=None):
-        super(GraphQlLister, self).__init__(app, app_args, cmd_name=cmd_name)
-        self.connect( SDF_IRIS_URI )
-
-    def connect(self, graphql_uri, get_schema=False ):
+    def connectGraphQl(self, graphql_uri=SDF_IRIS_URI, get_schema=False ):
         self.transport = AIOHTTPTransport(url=graphql_uri)
         self.client = Client(transport=self.transport, fetch_schema_from_transport=get_schema)
 
     def query(self, query, var={} ):
         return self.client.execute( gql(query), variable_values=var )
+
+
+class GraphQlLister(Lister, GraphQlClient):
+    "Example of how to fetch data from graphql and dump output as a table"
+    LOG = logging.getLogger(__name__)
+
+    def __init__(self, app, app_args, cmd_name=None):
+        super(GraphQlLister, self).__init__(app, app_args, cmd_name=cmd_name)
+        self.connectGraphQl()
 
 
