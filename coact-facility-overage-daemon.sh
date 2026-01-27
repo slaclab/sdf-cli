f625554cce2b15f5eb11f53288325c4aa4567a89
yee379 <yee379@gmail.com>
2026-01-26 23:04:47 -0800

    fix: updates for click conversion

--- coact-facility-overage-daemon.sh
+++ coact-facility-overage-daemon.sh
@@ -5,6 +5,6 @@ export SDF_COACT_URI=coact.slac.stanford.edu:443/graphql-service
 
 while [ 1 ]; do
     date
-    ./venv/bin/python3  ./sdf_click.py coact overage --password-file ./etc/.secrets/password --windows 3 15 60 180 1440 | sh
+    ./venv/bin/python3  ./sdf_click.py coact overage --password-file ./etc/.secrets/password --windows 5 --windows 15 --windows 60 --windows 180 --windows 1440 --verbose |  sh
     sleep 300
 done
--- coact-reporegistration-daemon.sh
+++ coact-reporegistration-daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while [ 1 ]; do
-    SDF_COACT_URI=coact.slac.stanford.edu/graphql-service ./venv/bin/python3 ./sdf_click.py coactd reporegistration --username=sdf-bot --password-file=etc/.secrets/password
+    SDF_COACT_URI=coact.slac.stanford.edu/graphql-service ./venv/bin/python3 ./sdf_click.py coactd reporegistration --username=sdf-bot --password-file=etc/.secrets/password -vv
     sleep 1
 done
