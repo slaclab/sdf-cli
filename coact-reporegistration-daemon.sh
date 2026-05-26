23bb3415e6366c95656bf2e78ada39a077fe22ec
Ryan Waldheim <ryanw@slac.stanford.edu>
2026-05-26 11:14:43 -0700

    add --grouper-password-file to daemon commands

--- coact-reporegistration-daemon.sh
+++ coact-reporegistration-daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while [ 1 ]; do
-    SDF_COACT_URI=coact.slac.stanford.edu/graphql-service ./venv/bin/python3 ./sdf_click.py coactd reporegistration --username=sdf-bot --password-file=etc/.secrets/password -vv
+    SDF_COACT_URI=coact.slac.stanford.edu/graphql-service ./venv/bin/python3 ./sdf_click.py coactd reporegistration --username=sdf-bot --password-file=etc/.secrets/password --grouper-password-file ./etc/.secrets/grouper_password -vv
     sleep 1
 done
